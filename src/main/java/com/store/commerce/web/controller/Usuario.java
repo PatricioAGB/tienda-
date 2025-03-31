60c1d4ad54abd98aa0dfb09b99db7beb81a809bb
PatricioAGB <Razer81368@gmail.com>
2025-03-31 16:16:57 -0300

    Modelo Base de datos

---
+++ src/main/java/com/store/commerce/web/controller/Usuario.java
@@ -0,0 +1,13 @@
+package com.store.commerce.web.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class Usuario {
+
+    @GetMapping("/getusers")
+}
