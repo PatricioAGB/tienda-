82925f88637403c252b31d00b678f088cc1a431d
PatricioAGB <Razer81368@gmail.com>
2025-03-31 18:45:37 -0300

    base de datos models

---
+++ src/main/java/com/store/commerce/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.store.commerce.repository;
+
+
+import com.store.commerce.models.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository  extends JpaRepository<Usuario, Integer> {
+}
---
+++ src/main/java/com/store/commerce/services/UsuarioService.java
@@ -0,0 +1,14 @@
+package com.store.commerce.services;
+
+import com.store.commerce.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+
+}
